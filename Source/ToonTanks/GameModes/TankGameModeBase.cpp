76206461cf46f04f0194f0334a51c83f879fea35
Pf0n1X <bboypsyfly@gmail.com>
2020-11-29 23:26:55 +0200

    152 - Adding Health Functionality

--- Source/ToonTanks/GameModes/TankGameModeBase.cpp
+++ Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -13,6 +13,7 @@ void ATankGameModeBase::BeginPlay()
 void ATankGameModeBase::ActorDied(AActor* DeadActor) 
 {
     // Check what yupr og svyor died. If Turret, tally. If Player -> go to lose condition.
+    UE_LOG(LogTemp, Warning, TEXT("A Pawn Died."));
 }
 
 void ATankGameModeBase::HandleGameStart() 
