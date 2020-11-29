ade08213fd7887bd30f1661a935875ca321edf4b
Pf0n1X <bboypsyfly@gmail.com>
2020-11-29 22:30:01 +0200

    150 - Creating GameMode classes

---
+++ Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankGameModeBase.h"
+
+void ATankGameModeBase::BeginPlay() 
+{
+    // Get refences and game win/lose conditions.
+
+    // Call Handle GameStart() to initialize the start countdown, turret actiation, pawn check, etc.
+}
+
+void ATankGameModeBase::ActorDied(AActor* DeadActor) 
+{
+    // Check what yupr og svyor died. If Turret, tally. If Player -> go to lose condition.
+}
+
+void ATankGameModeBase::HandleGameStart() 
+{
+    // Initialize the start countdown, turret activation, pawn check, etc.
+    // Call Blueprint version GameStart();
+}
+
+void ATankGameModeBase::HandleGameOver(bool PlayerWon) 
+{
+    // See if the player has destroyed all the turrets, show win result.
+    // else if turret destroyed player show lose result.
+    // Call blueprint version GameOver(bool);
+}
